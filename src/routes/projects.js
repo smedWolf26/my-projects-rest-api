9f8c8e2f94e89e5b4e70415eb9cf5f7e380234fb
smedWolf26 <smedina.lambda@gmail.com>
2026-03-14 15:21:32 -0500

    added sendResource and send collection response

--- src/routes/projects.js
+++ src/routes/projects.js
@@ -1,11 +1,23 @@
 import { Hono } from 'hono'
-import { listProjects } from '../data/store.js'
+import { listProjects, getProjectById } from '../data/store.js'
+import { ApiError } from '../utils/errors.js'
+import { sendCollection, sendResource} from '../utils/response.js'
 
 const projects = new Hono()
 
 projects.get('/', (c) =>{
   const data = listProjects()
-  return c.json(data)
+  return sendCollection(c, data)
+})
+
+projects.get('/:id', (c) => {
+  const project = getProjectById(+c.req.param('id'))
+
+  if (!project) {
+    throw new ApiError(404, 'NOT_FOUND', 'Project not found.')
+  }
+
+  return sendResource(c, project)
 })
 
 export default projects
